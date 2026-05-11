7ab96b33e37f7b38a9c852c3baad59111c794bcd
Jeffin2 <jeffinpvp.games@gmail.com>
2026-05-11 16:01:30 -0300

    teste de valores

--- valores.js
+++ valores.js
@@ -7,7 +7,7 @@ async function carregar() {
   el.classList.add("update");
   setTimeout(() => el.classList.remove("update"), 300);
 
-  el.innerText = "R$ " + data.valor;
+  el.innerText = data.valor;
 }
 
 carregar();
